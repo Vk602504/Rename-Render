177e7c34f00547541c440a8196a318f9767f9377
Vk602504 <vikashkumarjalpura4@gmail.com>
2024-12-13 21:10:44 +0530

    Update config.py

--- config.py
+++ config.py
@@ -7,9 +7,9 @@ import re, os
 
 id_pattern = re.compile(r'^.\d+$') 
 
-API_ID = os.environ.get("API_ID", "")
+API_ID = os.environ.get("API_ID", "20838202")
 
-API_HASH = os.environ.get("API_HASH", "")
+API_HASH = os.environ.get("API_HASH", "fde3e78e3256ae13e868b39088c83838")
 
 BOT_TOKEN = os.environ.get("BOT_TOKEN", "") 
 
@@ -21,13 +21,13 @@ FORCE_SUB = os.environ.get("FORCE_SUB", "VJ_Botz")
 
 DB_NAME = os.environ.get("DB_NAME", "renamevjbot")     
 
-DB_URL = os.environ.get("DB_URL", "")
+DB_URL = os.environ.get("DB_URL", "mongodb+srv://vk0634912:OKMino2da3dSzlZB@cluster0.pdtg3.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
  
 FLOOD = int(os.environ.get("FLOOD", "10"))
 
 START_PIC = os.environ.get("START_PIC", "https://te.legra.ph/file/119729ea3cdce4fefb6a1.jpg")
 
-ADMIN = [int(admin) if id_pattern.search(admin) else admin for admin in os.environ.get('ADMIN', '5606411877').split()]
+ADMIN = [int(admin) if id_pattern.search(admin) else admin for admin in os.environ.get('ADMIN', '5387859888').split()]
 
 PORT = os.environ.get("PORT", "8080")
 
